0aaff93f537451b93977737f880ab69203589447
gimyboya <b.afatah@yahoo.com>
2021-11-08 11:09:00 +0800

    feat: footer

--- components/global/GlobalFooter.vue
+++ components/global/GlobalFooter.vue
@@ -1,161 +1,108 @@
 <template>
-  <footer class="text-gray-600 body-font w-full">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap md:text-left text-center order-first">
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-          <nav class="list-none mb-10">
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-            </li>
-          </nav>
+  <footer class="w-full bg-gray-800 body-font">
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="py-12 mx-12 sm:px-6 lg:py-16 lg:px-8">
+      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div class="space-y-8 xl:col-span-1">
+          <img class="h-10" src="https://tailwindui.com/img/logos/workflow-mark-gray-300.svg" alt="Company name" />
+          <p class="text-base text-gray-200">Educate. Empower. Improve</p>
         </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-          <nav class="list-none mb-10">
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-            </li>
-          </nav>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-          <nav class="list-none mb-10">
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-              <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-            </li>
-          </nav>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
-          <div
-            class="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start"
-          >
-            <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2 mb-2">
-              <label for="footer-field" class="leading-7 text-sm text-gray-600">Placeholder</label>
-              <input
-                type="text"
-                id="footer-field"
-                name="footer-field"
-                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
+        <div class="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
+          <div class="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <h3 class="text-sm font-semibold tracking-wider text-gray-400 uppercase">Solutions</h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Marketing </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Analytics </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Commerce </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Insights </a>
+                </li>
+              </ul>
             </div>
-            <button
-              class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded"
-            >
-              Subscribe
-            </button>
+            <div class="mt-12 md:mt-0">
+              <h3 class="text-sm font-semibold tracking-wider text-gray-400 uppercase">Support</h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Pricing </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Documentation </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Guides </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> API Status </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <p class="text-gray-500 text-sm mt-2 md:text-left text-center">
-            Bitters chicharrones fanny pack <br class="lg:block hidden" />waistcoat green juice
-          </p>
+          <div class="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <h3 class="text-sm font-semibold tracking-wider text-gray-400 uppercase">Company</h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> About </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Blog </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Jobs </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Press </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Partners </a>
+                </li>
+              </ul>
+            </div>
+            <div class="mt-12 md:mt-0">
+              <h3 class="text-sm font-semibold tracking-wider text-gray-400 uppercase">Legal</h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Claim </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Privacy </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-base text-gray-200 hover:text-gray-900"> Terms </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="bg-gray-100">
-      <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-        <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span class="ml-3 text-xl">An-Nadaa</span>
-        </a>
-        <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-          © 2021 An-Nadaa
-        </p>
-        <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a class="text-gray-500">
-            <svg
-              fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-            </svg>
-          </a>
-          <a class="ml-3 text-gray-500">
-            <svg
-              fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
-              ></path>
-            </svg>
-          </a>
-          <a class="ml-3 text-gray-500">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-            </svg>
-          </a>
-          <a class="ml-3 text-gray-500">
-            <svg
-              fill="currentColor"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="0"
-              class="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="none"
-                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-              ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
-            </svg>
+
+    <div class="text-gray-400 bg-gray-900">
+      <div class="container flex flex-col items-center px-5 py-12 mx-12 sm:flex-row">
+        <p class="text-sm sm:ml-6 sm:mt-0">© 2021 An-Nadaa. All right reserved.</p>
+        <span class="inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start">
+          <a v-for="social in socialIcons" :href="social.link" :key="social.link">
+            <Component class="w-6 h-6 mx-3" :is="social.icon" />
           </a>
         </span>
       </div>
@@ -164,5 +111,36 @@
 </template>
 
 <script>
-export default {}
+import { BrandTwitterIcon, BrandLinkedinIcon, BrandFacebookIcon, BrandYoutubeIcon } from 'vue-tabler-icons'
+
+export default {
+  components: {
+    BrandTwitterIcon,
+    BrandLinkedinIcon,
+    BrandFacebookIcon,
+    BrandYoutubeIcon,
+  },
+  data() {
+    return {
+      socialIcons: [
+        {
+          icon: BrandTwitterIcon,
+          link: 'https://twitter.com',
+        },
+        {
+          icon: BrandFacebookIcon,
+          link: 'https://facebook.com',
+        },
+        {
+          icon: BrandLinkedinIcon,
+          link: 'https://linkedin.com',
+        },
+        {
+          icon: BrandYoutubeIcon,
+          link: 'https://youtube.com',
+        },
+      ],
+    }
+  },
+}
 </script>
