ebfdfa9b9059970d113fa5b11416fd4c281b765c
gimy <b.afatah@yahoo.com>
2022-03-20 01:36:10 +0800

    feat: change campaign card

--- components/campaigns/CampaignCard.vue
+++ components/campaigns/CampaignCard.vue
@@ -1,34 +1,64 @@
 <template>
-  <NuxtLink class="h-full overflow-hidden" :to="localePath(`/campaigns/${campaign.id}`)">
-    <img
-      class="object-cover w-full mb-6 aspect-video h-96"
-      v-if="campaign.attributes.cover.data.attributes.url"
-      :src="campaign.attributes.cover.data.attributes.url"
-      alt="cover"
-    />
+  <NuxtLink
+    class="flex flex-col overflow-hidden rounded-lg shadow-lg hover:shadow-xl"
+    :to="localePath(`/campaigns/${campaign.id}`)">
+    <div class="flex-shrink-0">
+      <img
+        class="object-cover w-full h-64"
+        v-if="campaign.attributes.cover.data.attributes.url"
+        :src="campaign.attributes.cover.data.attributes.url"
+        alt="" />
+    </div>
+    <div class="flex flex-col justify-between flex-1 p-6 bg-white">
+      <div class="flex-1">
+        <span
+          v-for="(tag, i) in campaign.attributes.tags.data"
+          :key="i"
+          :class="`inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-primary-100 text-primary-800  ${
+            i === 0 ? '' : 'ltr:mr-4 rtl:ml-4 mx-2'
+          }`">
+          {{ tag.attributes.value }}
+        </span>
 
-    <div class="flex flex-col">
-      <div class="h-2 my-3 bg-gray-300">
+        <div class="block mt-2">
+          <p class="text-xl font-semibold text-gray-900">
+            {{ campaign.attributes.title }}
+          </p>
+          <p class="mt-3 text-base text-gray-500">
+            {{ campaign.attributes.description }}
+          </p>
+        </div>
+      </div>
+
+      <div class="h-2 my-3 bg-gray-300 group">
         <div class="h-2 bg-yellow-400" :style="`width: ${percentage}%`"></div>
       </div>
-      <div class="self-end text-gray-400 slashed-zero">
-        Goal: {{ currencySymbol }} {{ Intl.NumberFormat().format(campaign.attributes.goal) }}
+
+      <div class="flex items-center mt-6">
+        <!-- <div class="flex-shrink-0">
+                <a :href="post.author.href">
+                  <span class="sr-only">{{ post.author.name }}</span>
+                  <img class="w-10 h-10 rounded-full" :src="post.author.imageUrl" alt="" />
+                </a>
+              </div> -->
+        <div>
+          <!-- <p class="text-sm font-medium text-gray-900">
+                  <div class="hover:underline">
+                    {{ percentage }} %
+                  </div>
+                </p> -->
+          <div class="flex space-x-1 text-base text-gray-500">
+            <!-- <time :datetime="post.datetime">
+                    {{ post.date }}
+                  </time> -->
+            <span class="font-semibold">
+              {{ currencySymbol }} {{ Intl.NumberFormat().format(campaign.attributes.raised) }}
+            </span>
+            <span> of </span>
+            <span> {{ currencySymbol }} {{ Intl.NumberFormat().format(campaign.attributes.goal) }} Raised </span>
+          </div>
+        </div>
       </div>
-    </div>
-    <div>
-      <h1 class="mb-3 text-2xl font-medium text-gray-900 font-texts">{{ campaign.attributes.title }}</h1>
-      <p class="mb-6 leading-relaxed text-gray-500 line-clamp-3">
-        {{ campaign.attributes.description }}
-      </p>
-      <span
-        v-for="(tag, i) in campaign.attributes.tags.data"
-        :key="i"
-        :class="`inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-primary-100 text-primary-800 mx-2 ${
-          i === 0 ? '' : 'ltr:mr-4 rtl:ml-4'
-        }`"
-      >
-        {{ tag.attributes.value }}
-      </span>
     </div>
   </NuxtLink>
 </template>
