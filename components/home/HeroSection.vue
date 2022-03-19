c7222d4796024a6889c0deed4cce2dcf459bd88d
gimy <b.afatah@yahoo.com>
2022-03-19 20:20:15 +0800

    fix: better structure for the hero section

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -2,13 +2,13 @@
   <section class="bg-contain lg:h-screen" :style="`background-image: url(${bgUrl})`">
     <div v-show="!showPlayer" class="w-full pt-16 pb-20 mx-auto text-center lg:mx-24 max-w-7xl lg:py-48 lg:text-left">
       <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
-        <h1 class="text-4xl tracking-tight text-white font-headers sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+        <h1 class="text-4xl text-white font-headers sm:text-5xl md:text-6xl xl:text-7xl">
           <span class="block xl:inline">Build your</span>
           <span class="block xl:inline">Hereafter today</span>
         </h1>
         <p class="max-w-md mx-auto mt-3 text-lg text-gray-100 sm:text-xl md:mt-5 md:max-w-3xl">
-          An-Nadaa is bringing a huge positive impact for the underprivileged communities in Nigeria through our
-          projects.
+          You can change the lives of millions by supporting educational and welfare projects of An-Nadaa that are
+          bringing a huge positive impact to the community.
         </p>
         <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
           <div class="rounded-md shadow">
