412ac1f54fa19884600882266511ff0780bf8abf
gimy <b.afatah@yahoo.com>
2022-03-19 20:21:02 +0800

    fix: improve stats section

--- components/home/StatsSection.vue
+++ components/home/StatsSection.vue
@@ -17,7 +17,7 @@
       class="max-w-4xl px-4 mx-auto lg:mx-24 sm:px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-cols-2 xl:grid-flow-col-dense xl:gap-x-8">
       <div class="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24">
         <h2 class="text-sm font-semibold tracking-wide uppercase text-primary-300">Valuable Metrics</h2>
-        <p class="mt-3 text-3xl font-extrabold text-white">Get actionable data that will help grow your business</p>
+        <h3 class="mt-3 text-5xl text-white">Get actionable data that will help grow your business</h3>
         <p class="mt-5 text-lg text-gray-300">
           Rhoncus sagittis risus arcu erat lectus bibendum. Ut in adipiscing quis in viverra tristique sem. Ornare
           feugiat viverra eleifend fusce orci in quis amet. Sit in et vitae tortor, massa. Dapibus laoreet amet lacus
