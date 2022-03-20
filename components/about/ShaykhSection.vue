87a4515bf27c8cf295e6d6e2b4244b4ee342d555
gimy <b.afatah@yahoo.com>
2022-03-20 13:29:37 +0800

    fix: content breaking design

--- components/about/ShaykhSection.vue
+++ components/about/ShaykhSection.vue
@@ -51,7 +51,45 @@
             </figure>
           </div>
         </div>
-        <div class="mt-8 lg:mt-0" v-html="shaykhSection.description">
+        <div class="mt-8 lg:mt-0">
+          <div class="mx-auto text-base max-w-prose lg:max-w-none">
+            <p class="text-lg text-gray-500">
+              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+              pellentesque. Blandit amet, sed aenean erat arcu morbi.
+            </p>
+          </div>
+          <div class="mx-auto mt-5 prose text-gray-500 prose-primary lg:max-w-none lg:row-start-1 lg:col-start-1">
+            <p>
+              Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium
+              arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
+            </p>
+            <p>
+              Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet
+              fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.
+            </p>
+            <p>
+              Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.
+            </p>
+            <ul role="list">
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
+              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+            </ul>
+            <p>
+              Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit
+              faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim
+              sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
+            </p>
+            <!-- <h3>How we helped</h3>
+            <p>
+              Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam. Montes,
+              magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa, lectus.
+              Diam rutrum posuere donec ultricies non morbi. Mi a platea auctor mi.
+            </p>
+            <p>
+              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+              pellentesque. Blandit amet, sed aenean erat arcu morbi.
+            </p> -->
           </div>
         </div>
       </div>
