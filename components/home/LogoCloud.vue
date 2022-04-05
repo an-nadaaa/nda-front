ab317ad44e753fad7d9bbbeba254d0f5eec4fda1
gimy <b.afatah@yahoo.com>
2022-04-05 13:25:32 +0800

    feat: add logos

--- components/home/LogoCloud.vue
+++ components/home/LogoCloud.vue
@@ -2,22 +2,22 @@
   <div class="bg-gray-800">
     <div class="px-4 pt-12 pb-24 sm:px-6 lg:px-8">
       <h3 class="mx-auto mb-12 text-4xl text-center text-gray-300">Trusted by</h3>
-      <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-        <div class="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
-          <img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />
+      <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-6">
+        <div class="flex justify-center col-span-1 md:col-span-2 lg:col-span-2">
+          <img class="h-12" src="~/assets/img/logos/Global.svg" alt="Tuple" />
         </div>
-        <div class="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
-          <img class="h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" />
+        <div class="flex justify-center col-span-1 md:col-span-2 lg:col-span-2">
+          <img class="h-12" src="~/assets/img/logos/Quraany.svg" alt="Mirage" />
         </div>
-        <div class="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
-          <img class="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" />
+        <div class="flex justify-center col-span-1 md:col-span-2 lg:col-span-2">
+          <img class="h-12" src="~/assets/img/logos/Nmic.svg" alt="StaticKit" />
         </div>
-        <div class="flex justify-center col-span-1 md:col-span-3 lg:col-span-1">
+        <!-- <div class="flex justify-center col-span-1 md:col-span-3 lg:col-span-1">
           <img class="h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Transistor" />
         </div>
         <div class="flex justify-center col-span-2 md:col-span-3 lg:col-span-1">
           <img class="h-12" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation" />
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
