424d7f1df9af28862039e1e21abcdf49119ed8f6
gimy <b.afatah@yahoo.com>
2022-04-05 12:51:47 +0800

    feat: integrate mailchimp

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -8,18 +8,31 @@
         </p>
       </div>
       <div class="mt-12 sm:w-full sm:max-w-md lg:mt-0 lg:ml-8 lg:flex-1">
-        <form class="sm:flex">
-          <label for="email-address" class="sr-only">Email address</label>
+        <form
+          class="sm:flex"
+          action="https://an-nadaa.us7.list-manage.com/subscribe/post?u=6121dee1d3173d0593a42fa31&amp;id=c61f24d44c"
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          target="_blank">
+          <label for="mce-EMAIL" class="sr-only">Email address</label>
           <input
-            id="email-address"
-            name="email-address"
+            id="mce-EMAIL"
+            name="EMAIL"
             type="email"
             autocomplete="email"
             required
             class="w-full px-5 py-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary-700 focus:ring-white"
             placeholder="Enter your email" />
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px" aria-hidden="true">
+            <input type="text" name="b_6121dee1d3173d0593a42fa31_c61f24d44c" tabindex="-1" value="" />
+          </div>
           <button
             type="submit"
+            value="Subscribe"
+            name="subscribe"
+            id="mc-embedded-subscribe"
             class="flex items-center justify-center w-full px-5 py-3 mt-3 text-base font-medium text-white border border-transparent rounded-md bg-primary-600 hover:bg-primary-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0">
             Subscribe
           </button>
